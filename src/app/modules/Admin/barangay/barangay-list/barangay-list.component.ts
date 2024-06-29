4165fb864dbe71941df58c208f99151a3fb78d12
rocso <rocso@DESKTOP-FTA3M8O>
2024-06-29 15:04:06 +0800

    Add Delete Barangay Function

--- src/app/modules/Admin/barangay/barangay-list/barangay-list.component.ts
+++ src/app/modules/Admin/barangay/barangay-list/barangay-list.component.ts
@@ -103,4 +103,34 @@ export class BarangayListComponent implements OnInit {
       }
     )
   }
+
+  onDelete(barangayId: string): void {
+    Swal.fire({
+      title: "Are you sure?",
+      text: "You won't be able to revert this!",
+      icon: "warning",
+      showCancelButton: true,
+      confirmButtonColor: "#3085d6",
+      cancelButtonColor: "#d33",
+      confirmButtonText: "Yes, delete it!"
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.barangayService.delete(barangayId)
+          .subscribe({
+            next: (response) => {
+              this.fetchBarangay();
+            },
+            error: (error) => {
+              Swal.fire({
+                icon: "error",
+                title: "Oops...",
+                text: "Something went wrong!",
+              });
+            }
+          });
+      } else {
+        console.log('Error Occured while Deleting ...');
+      }
+    });
+  }
 }
