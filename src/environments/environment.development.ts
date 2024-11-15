fea818250f4c748ba7bd86936592b71cc2e894d1
reynand <rocso58@gmail.com>
2024-11-15 09:19:51 +0800

    Change ip to Prod

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -1,9 +1,6 @@
 export const environment = {
     production: false,
-    // apiBaseUrl: 'http://surigaodelsur.gov.ph:2024'
-    apiBaseUrl: 'https://localhost:7044'
+    apiBaseUrl: 'http://surigaodelsur.gov.ph:2024'
+    //apiBaseUrl: 'https://localhost:7044'
 };
 
-
-
-// development: https://localhost:7044
